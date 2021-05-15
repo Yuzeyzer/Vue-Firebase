9d2384223fe50ce8bd6817e408121225c92a6a94
John Doe <kurbanof.atabek@gmail.com>
2021-05-16 04:04:23 +0600

    template refs, getting access to the DOM and manipulate element

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# Vue-Firebase
-We learned how to dynamically render variables in Vue, how to use Vue data() and Vue methods
+# vue-firebase
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <h1>{{title}}</h1>
+  <input type="text" ref="name">
+  <button @click="handleClick">Click me!</button>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data: function () {
+    return {
+      title: 'Слава богу я атеист'
+    }
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name)
+      const input = this.$refs.name;
+      input.classList.add('active')
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
