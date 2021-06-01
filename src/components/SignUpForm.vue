7c6bf7c06df796b03f5e6e08187b2156b422c806
John Doe <kurbanof.atabek@gmail.com>
2021-06-01 14:23:00 +0600

    add skill, delete skill methods, keyboard Events & Modifiers

--- src/components/SignUpForm.vue
+++ src/components/SignUpForm.vue
@@ -9,28 +9,20 @@
       <option value="developer">Web Developer</option>
       <option value="designer">Web Designer</option>
     </select>
-		<div class="terms">
-			<input type="checkbox" required v-model="terms">
-			<label for="">Accept terms and conditions</label>
-		</div>
-		<div>
-			<input type="checkbox" value="Сейтек" v-model="names">
-			<label for="">Сейтек</label>
-		</div>
-			<div>
-			<input type="checkbox" value="Бекжан" v-model="names">
-			<label for="">Бекжан</label>
-		</div>
-			<div>
-			<input type="checkbox" value="Аки" v-model="names">
-			<label for="">Аки</label>
-		</div>
+    <label for="">Skills:</label>
+    <input type="text" v-model="tempSkill" @keyup.alt="addSkill" />
+    <div v-for="skill in skills" :key="skill" class="pill">
+      <span @click="deleteSkill(skill)"> {{ skill }}</span>
+    </div>
+    <div class="terms">
+      <input type="checkbox" required v-model="terms" />
+      <label for="">Accept terms and conditions</label>
+    </div>
   </form>
   <p>Email: {{ email }}</p>
   <p>Password: {{ password }}</p>
   <p>Role: {{ role }}</p>
-	<p>Terms: {{ terms }}</p>
-	<p>Names: {{ names }}</p>
+  <p>Terms: {{ terms }}</p>
 </template>
 
 <script>
@@ -40,9 +32,23 @@ export default {
       email: "",
       password: "",
       role: "designer",
-			terms: false,
-			names: []
+      terms: false,
+      tempSkill: "",
+      skills: [],
     };
+  },
+  methods: {
+    addSkill(e) {
+      if (e.key === "," && this.tempSkill) {
+        if (!this.skills.includes(this.tempSkill)) {
+          this.skills.push(this.tempSkill);
+        }
+        this.tempSkill = "";
+      }
+    },
+		deleteSkill(skill) {
+			this.skills = this.skills.filter(item => item !== skill)
+		}
   },
 };
 </script>
@@ -76,10 +82,22 @@ select {
   color: #555;
 }
 input[type="checkbox"] {
-	position: relative;
-	top: 2px;
-	display: inline-block;
-	width: 16px;
-	margin: 0 10px 0 0;
+  position: relative;
+  top: 2px;
+  display: inline-block;
+  width: 16px;
+  margin: 0 10px 0 0;
+}
+.pill {
+  display: inline-block;
+  margin: 20px 10px 0 0;
+  padding: 6px 12px;
+  background-color: #eee;
+  border-radius: 20px;
+  font-size: 12px;
+  letter-spacing: 1px;
+  font-weight: 700;
+  color: #777;
+  cursor: pointer;
 }
 </style>
