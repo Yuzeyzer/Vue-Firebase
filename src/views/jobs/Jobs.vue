485453c98b5054a166e41b76f8054752b677017b
John Doe <kurbanof.atabek@gmail.com>
2021-06-06 01:57:14 +0600

    some styles for jobs component

--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -1,6 +1,6 @@
 <template>
   <h1>Вакансии</h1>
-  <div v-for="job in jobs" :key="job.id">
+  <div v-for="job in jobs" :key="job.id" class="job">
     <router-link :to="{ name: 'JobsDetails', params: { id: job.id } }">
       <h2>{{ job.title }}</h2>
     </router-link>
@@ -34,4 +34,19 @@ export default {
 </script>
 
 <style>
+.job h2 {
+  background-color: #f4f4f4;
+  padding: 20px;
+  border-radius: 10px;
+  margin: 10px auto;
+  max-width: 600px;
+  cursor: pointer;
+  color: #444;
+}
+.job h2:hover {
+	background-color: #ddd;
+}
+.job a {
+	text-decoration: none;
+}
 </style>
