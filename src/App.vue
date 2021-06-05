a0388225c6ebc5fa07b13dcfa2a3a535546522f7
John Doe <kurbanof.atabek@gmail.com>
2021-06-06 03:03:46 +0600

    new project start, deleted no - need files

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,19 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link :to="{ name: 'About' }">About</router-link>
-    <router-link :to="{ name: 'Jobs' }">Вакансии</router-link>
-  </div>
-  <button @click="redirect">Redirect</button>
-  <button @click="back">Go back</button>
-  <button @click="forward">Go forward</button>
   <router-view />
 </template>
 
 <script>
-export default {
-  methods: {
-    redirect() {
-       this.$router.push({name: 'Home'});
-    },
-    back() {
-      this.$router.go(-1);
-    },
-    forward() {
-      this.$router.go(1);
-    },
-  },
-};
+export default {};
 </script>
 
 <style>
+body {
+  background-color: #f2f2f2;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-  text-decoration: none;
-  padding: 10px;
-}
-
-#nav a.router-link-exact-active {
-  color: #fff;
-  background-color: crimson;
-}
-button {
-  margin: 0 10px;
-  padding: 10px;
-  border: none;
-  border-radius: 4px;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,42 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
-import About from '../views/About.vue';
-import Jobs from '../views/jobs/Jobs.vue';
-import JobsDetails from '../views/jobs/JobsDetails';
-import NotFound from '../views/NotFound';
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home,
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: () => import('../views/About.vue'),
-  },
-  {
-    path: '/jobs',
-    name: 'Jobs',
-    component: Jobs,
-  },
-  {
-    path: '/jobs/:id',
-    name: 'JobsDetails',
-    component: JobsDetails,
-    props: true,
-  },
-  // redirect
-  {
-    path: '/all-jobs',
-    redirect: '/jobs'
-  },
-  // catchall 404
-  {
-    path: '/:catchAll(.*)',
-    name: 'NotFound',
-    component: NotFound,
   }
 ];
 
