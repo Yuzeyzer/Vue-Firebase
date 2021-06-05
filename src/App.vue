37cc99d58fb057145a55dd531f22f24a04c6d873
John Doe <kurbanof.atabek@gmail.com>
2021-06-06 03:22:54 +0600

    created Project component, some styling

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,8 @@ body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  max-width: 600px;
+  margin: 0 auto;
   color: #2c3e50;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,30 @@
 <template>
   <div class="home">
-    <h1>Homepage</h1>
+    <div v-if="projects.length">
+      <div v-for="project in projects" :key="project.id">
+        <Project :project="project" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Project from "@/components/Project.vue";
 // @ is an alias to /src
 
 export default {
   name: "Home",
-  components: {},
+  components: { Project },
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/projects")
+      .then((response) => response.json())
+      .then((data) => (this.projects = data))
+      .catch((error) => console.error(error));
+  },
 };
 </script>
