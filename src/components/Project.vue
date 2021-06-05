e662754263804b6c474c9dcc2e3222cc26a6a064
John Doe <kurbanof.atabek@gmail.com>
2021-06-06 03:30:04 +0600

    installed material-icons, used some icons (edit, delete, done) and bit of styling

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -2,6 +2,11 @@
   <div class="project">
     <div class="actions">
       <h3 @click="toggleShowDetails">{{ project.title }}</h3>
+      <div class="icons">
+        <span class="material-icons">edit</span>
+        <span class="material-icons">delete</span>
+        <span class="material-icons">done</span>
+      </div>
     </div>
     <div v-if="showDetails" class="details">
       <p>{{ project.details }}</p>
@@ -36,5 +41,19 @@ export default {
 }
 .project h3 {
   cursor: pointer;
+}
+.actions {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.icons .material-icons {
+  font-size: 24px;
+  margin-left: 10px;
+  color: #bbb;
+  cursor: pointer;
+}
+.material-icons:hover {
+  color: #777;
 }
 </style>
