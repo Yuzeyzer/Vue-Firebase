eb5b9bcea66e552e1806fdfd532812dbb4358aad
John Doe <kurbanof.atabek@gmail.com>
2021-06-06 02:11:43 +0600

    Vue router web history

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,30 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link :to="{name:'About'}">About</router-link>
-     <router-link :to="{name:'Jobs'}">Вакансии</router-link>
+    <router-link :to="{ name: 'About' }">About</router-link>
+    <router-link :to="{ name: 'Jobs' }">Вакансии</router-link>
   </div>
-  <router-view/>
+  <button @click="redirect">Redirect</button>
+  <button @click="back">Go back</button>
+  <button @click="forward">Go forward</button>
+  <router-view />
 </template>
+
+<script>
+export default {
+  methods: {
+    redirect() {
+       this.$router.push({name: 'Home'});
+    },
+    back() {
+      this.$router.go(-1);
+    },
+    forward() {
+      this.$router.go(1);
+    },
+  },
+};
+</script>
 
 <style>
 #app {
@@ -30,5 +49,11 @@
 #nav a.router-link-exact-active {
   color: #fff;
   background-color: crimson;
+}
+button {
+  margin: 0 10px;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
 }
 </style>
