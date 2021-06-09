7e2ba403bce3ff43b9b659c6644fde689b648f3f
John Doe <kurbanof.atabek@gmail.com>
2021-06-10 01:11:20 +0600

    created EditProject view, fetching data for editing (Not finished)

---
+++ src/views/EditProject.vue
@@ -0,0 +1,35 @@
+<template>
+  <form>
+    <label for="">Название задачи:</label>
+    <input type="text" required v-model="title" />
+    <label for="">Описание:</label>
+    <textarea required v-model="details"></textarea>
+    <button>Редактировать задачу</button>
+  </form>
+</template>
+
+<script>
+export default {
+  props: ["id"],
+  data() {
+    return {
+      title: "",
+      details: "",
+      uri: "http://localhost:3000/projects/" + this.id,
+    };
+  },
+  mounted() {
+    const fetchData = async () => {
+      const response = await fetch(this.uri);
+      const json = await response.json();
+      console.log(json);
+      this.title = json.title;
+      this.details = json.details;
+    };
+    fetchData();
+  },
+};
+</script>
+
+<style>
+</style>
