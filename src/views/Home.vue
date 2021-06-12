a54d611eac7a04b82b513564456283057c355565
John Doe <kurbanof.atabek@gmail.com>
2021-06-13 03:58:03 +0600

    Refs vs Reactives in Vue - 3, differences between them

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,38 @@
 <template>
-  <p ref="p">Меня зовут {{ name }} и мне {{ age }} лет</p>
-  <button @click="handleClick">Нажми на меня</button>
-  <button @click="age++">Увеличить возраст</button>
-  <input type="text" v-model="name">
+  <div class="home">
+    <h1>Home</h1>
+    <h2>Refs</h2>
+    <p>{{ person.name }} - {{ person.age }}</p>
+    <button @click="updatePerson">Обновить пользователя</button>
+    <h2>Reactivity</h2>
+    <p>{{ person2.name }} - {{ person2.age }}</p>
+    <button @click="updatePerson2">Обновить пользователя</button>
+  </div>
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref, reactive } from "vue";
 
 export default {
   name: "Home",
   setup() {
-    const name = ref("Абдушукур");
-    const age = ref(99);
+    const person = ref({ name: "Бекжан", age: 12 });
+    const person2 = reactive({ name: "Сейтек", age: 13 });
 
-    const p = ref(null);
+    const name = reactive('Али'); // Ошибка, функции reactive не стоит 
+                                  // принимать примитивный тип данных,он не будет реактивным 
 
-    const handleClick = () => {
-     name.value = 'Тентек';
-     age.value = '12'
+    const updatePerson = () => {
+      person.value.age = 19;
     };
 
-    return { name, age, handleClick, p };
+    const updatePerson2 = () => {
+      person2.age = 19;
+
+      name = 'Байкуш' // значение не будет динамически обновлено
+    };
+
+    return { person, person2, updatePerson, updatePerson2 };
   },
 };
 </script>
