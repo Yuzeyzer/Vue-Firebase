5e01d818ac85eec81fac8c9d81762d657ab1eae1
John Doe <kurbanof.atabek@gmail.com>
2021-06-13 04:08:06 +0600

    Computed properties in Vue - 3, Composition API

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,38 +1,34 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <h2>Refs</h2>
-    <p>{{ person.name }} - {{ person.age }}</p>
-    <button @click="updatePerson">Обновить пользователя</button>
-    <h2>Reactivity</h2>
-    <p>{{ person2.name }} - {{ person2.age }}</p>
-    <button @click="updatePerson2">Обновить пользователя</button>
+    <input type="text" v-model="search" />
+    <div v-for="name in matchingNames" :key="name">
+      {{ name }}
+    </div>
   </div>
 </template>
 
 <script>
-import { ref, reactive } from "vue";
+import { ref, computed } from "vue";
 
 export default {
   name: "Home",
   setup() {
-    const person = ref({ name: "Бекжан", age: 12 });
-    const person2 = reactive({ name: "Сейтек", age: 13 });
+    const names = ref([
+      "Сейтек",
+      "Бекжан",
+      "Али",
+      "Аки",
+      "Аэлина",
+      "Кочкорбек",
+    ]);
+    const search = ref("");
 
-    const name = reactive('Али'); // Ошибка, функции reactive не стоит 
-                                  // принимать примитивный тип данных,он не будет реактивным 
+    const matchingNames = computed(() => {
+      return names.value.filter((name) => name.includes(search.value));
+    });
 
-    const updatePerson = () => {
-      person.value.age = 19;
-    };
-
-    const updatePerson2 = () => {
-      person2.age = 19;
-
-      name = 'Байкуш' // значение не будет динамически обновлено
-    };
-
-    return { person, person2, updatePerson, updatePerson2 };
+    return { names, search, matchingNames };
   },
 };
 </script>
