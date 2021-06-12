1b10021e523b296d864069cac5c8c40891878f45
John Doe <kurbanof.atabek@gmail.com>
2021-06-13 01:30:55 +0600

    Copmposition API ref's

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,25 @@
 <template>
-  <p>Меня зовут {{ name }} и мне {{ age }} лет</p>
+  <p ref="p">Меня зовут {{ name }} и мне {{ age }} лет</p>
   <button @click="handleClick">Нажми на меня</button>
 </template>
 
 <script>
+import { ref } from "vue";
+
 export default {
   name: "Home",
   setup() {
     let name = "Абдушукур";
     let age = 99;
 
+    const p = ref(null);
+
     const handleClick = () => {
       console.log("Ты нажал на меня!!!");
+      console.log(p.value, 'мы сохранили элемент в переменной p');
     };
 
-    return { name, age, handleClick };
+    return { name, age, handleClick, p };
   },
 };
 </script>
