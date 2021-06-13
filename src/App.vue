200ed87c9a95220705db4402d5de76d607336361
John Doe <kurbanof.atabek@gmail.com>
2021-06-13 08:52:12 +0600

    created Navbar Component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <template>
+  <Navbar />
   <router-view />
 </template>
 
 <script>
-import Home from "@/views/Home.vue";
+import Navbar from "@/components/Navbar.vue";
 
 export default {
   name: "App",
-  components: { Home },
+  components: { Navbar },
 };
 </script>
 
