854583f758b67eb2709bd6b204d3152663c63f85
John Doe <kurbanof.atabek@gmail.com>
2021-06-13 07:42:54 +0600

    created getPost composable, created Details.vue for singlePost

---
+++ src/composables/getPost.js
@@ -0,0 +1,24 @@
+import { ref } from 'vue';
+
+const getPost = (id) => {
+  const post = ref(null);
+  const error = ref(null);
+
+  const fetchBody = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/posts/' + id);
+
+      if (!response.ok) {
+        throw Error('No data available');
+      }
+
+      post.value = await response.json();
+    } catch (e) {
+      error.value = e.message;
+      console.log(error.value);
+    }
+  };
+  return { post, error, fetchBody };
+};
+
+export default getPost;
---
+++ src/views/Details.vue
@@ -0,0 +1,36 @@
+<template>
+  <div v-if="error">{{ error }}</div>
+  <div v-if="post" class="post">
+    <h3>{{ post.title }}</h3>
+    <p class="pre">{{ post.body }}</p>
+  </div>
+</template>
+
+<script>
+import getPost from "@/composables/getPost";
+
+export default {
+  props: ["id"],
+  setup(props) {
+    const { post, error, fetchBody } = getPost(props.id);
+    fetchBody();
+    return { post, error };
+  },
+};
+</script>
+
+<style>
+.post {
+  max-width: 1200px;
+  margin: 0 auto;
+}
+
+.props p {
+  color: #444;
+  line-height: 1.5em;
+  margin-top: 40px;
+}
+.pre {
+  white-space: pre-wrap;
+}
+</style>
