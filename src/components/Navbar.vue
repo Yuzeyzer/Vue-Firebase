ea459f4f6755d21962e728b8f8bdea7578a0b1bf
John Doe <kurbanof.atabek@gmail.com>
2021-06-28 18:56:06 +0600

    created useLogout composable

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,29 +4,44 @@
       <p>Добро Пожаловать</p>
       <p class="email">Вы вошли в аккаунт с почтой ...</p>
     </div>
-    <button>Выйти</button>
+    <button @click="handleClick">Выйти</button>
   </nav>
 </template>
 
 <script>
-export default {};
+import useLogout from "@/composables/useLogout";
+export default {
+  components: {},
+  setup() {
+    const { error, logout } = useLogout();
+
+    const handleClick = async () => {
+      await logout();
+      if (!error.value) {
+        console.log("Пользователь разлогинен");
+      }
+    };
+
+    return { error, handleClick };
+  },
+};
 </script>
 
 <style>
-  nav {
-    padding: 20px;
-    border-bottom: 1px solid #eee;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-  nav p {
-    margin: 2px auto;
-    font-size: 16px;
-    color: #444;
-  }
-  nav p.email {
-    font-size: 14px;
-    color: #999;
-  }
+nav {
+  padding: 20px;
+  border-bottom: 1px solid #eee;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+nav p {
+  margin: 2px auto;
+  font-size: 16px;
+  color: #444;
+}
+nav p.email {
+  font-size: 14px;
+  color: #999;
+}
 </style>
