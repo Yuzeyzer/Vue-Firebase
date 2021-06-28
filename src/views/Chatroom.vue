4ef62bf8882a907c2cb1027ac00ec03426fd9212
John Doe <kurbanof.atabek@gmail.com>
2021-06-29 01:57:05 +0600

    redirectings user after logout

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -6,9 +6,21 @@
 </template>
 
 <script>
+import { watch } from "@vue/runtime-core";
 import Navbar from "../components/Navbar.vue";
+import getUser from "@/composables/getUser";
+import { useRouter } from "vue-router";
 export default {
   components: { Navbar },
+  setup() {
+    const { user } = getUser();
+    const router = useRouter();
+    watch(user,() => {
+      if (!user.value) {
+        router.push("/");
+      }
+    });
+  },
 };
 </script>
 
