ea71c6965221f797d8194798cc29dfc2d12d7c0e
John Doe <kurbanof.atabek@gmail.com>
2021-06-28 17:33:02 +0600

    created navbar component and import it in Chatroom.vue

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 <script>
 export default {
   name: "App",
-  components: {},
+  components: { },
 };
 </script>
 
---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav>
+    <div>
+      <p>Добро Пожаловать</p>
+      <p class="email">Вы вошли в аккаунт с почтой ...</p>
+    </div>
+    <button>Выйти</button>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+  nav {
+    padding: 20px;
+    border-bottom: 1px solid #eee;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  nav p {
+    margin: 2px auto;
+    font-size: 16px;
+    color: #444;
+  }
+  nav p.email {
+    font-size: 14px;
+    color: #999;
+  }
+</style>
