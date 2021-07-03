e5f4009ba59259265161e8a19b3639ca78449fee
John Doe <kurbanof.atabek@gmail.com>
2021-07-03 18:46:29 +0600

    add logout functionality in Navbar component

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,45 +1,60 @@
 <template>
   <div class="navbar">
     <nav>
-			<img src="@/assets/logo.png" />
+      <img src="@/assets/logo.png" />
       <h1><router-link :to="{ name: 'Home' }">Muic</router-link></h1>
       <div class="links">
-        <button>Log Out</button>
-				<router-link class="btn" :to="{ name: 'Signup' }">Sign Up</router-link>
-				<router-link class="btn" :to="{ name: 'Login' }">Login</router-link>
+        <button @click="handleLogOut">Log Out</button>
+        <router-link class="btn" :to="{ name: 'Signup' }">Sign Up</router-link>
+        <router-link class="btn" :to="{ name: 'Login' }">Login</router-link>
       </div>
     </nav>
   </div>
 </template>
 
 <script>
-export default {};
+import useLogout from "../composables/useLogout";
+import { useRouter } from "vue-router";
+export default {
+  setup() {
+    const { logout } = useLogout();
+    const router = useRouter();
+
+    const handleLogOut = async () => {
+      await logout();
+      console.log("User is logged out");
+      router.push({ name: "Login" });
+    };
+    return { handleLogOut };
+  },
+};
 </script>
 
 <style scoped>
-  .navbar {
-    padding: 16px 10px;
-    margin-bottom: 60px;
-    background: white;
-  }
-  nav {
-    display: flex;
-    align-items: center;
-    max-width: 1200px;
-    margin: 0 auto;
-  }
-  nav img {
-    max-height: 60px;
-		object-fit: contain
-  }
-  nav h1 {
-    margin-left: 20px;
-  }
-  nav .links {
-    margin-left: auto;
-  }
-  nav .links a, button {
-    margin-left: 16px;
-    font-size: 14px;
-  }
+.navbar {
+  padding: 16px 10px;
+  margin-bottom: 60px;
+  background: white;
+}
+nav {
+  display: flex;
+  align-items: center;
+  max-width: 1200px;
+  margin: 0 auto;
+}
+nav img {
+  max-height: 60px;
+  object-fit: contain;
+}
+nav h1 {
+  margin-left: 20px;
+}
+nav .links {
+  margin-left: auto;
+}
+nav .links a,
+button {
+  margin-left: 16px;
+  font-size: 14px;
+}
 </style>
