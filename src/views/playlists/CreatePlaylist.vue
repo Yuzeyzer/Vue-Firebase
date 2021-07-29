2f22dd3f4507d0f19faaedf57f3e0e211940a8ac
John Doe <kurbanof.atabek@gmail.com>
2021-07-29 17:21:57 +0600

    add styles for createPlaylist view

--- src/views/playlists/CreatePlaylist.vue
+++ src/views/playlists/CreatePlaylist.vue
@@ -1,20 +1,48 @@
 <template>
-  <form>
-    <h4>Create New Playlist</h4>
-    <input type="text" placeholder="Playlist title" required v-model="title">
-    <textarea placeholder="Playlist description" required v-model="description"></textarea>
-    <label>Upload playlist cover image</label>
-    <input type="file">
-    <button>Create</button>
-  </form>
+	<form @submit.prevent="handleSubmit">
+		<h4>Create New Playlist</h4>
+		<input type="text" placeholder="Playlist title" required v-model="title" />
+		<textarea
+			placeholder="Playlist description"
+			required
+			v-model="description"
+		></textarea>
+		<label>Upload playlist cover image</label>
+		<input type="file" />
+		<button>Create</button>
+	</form>
 </template>
 
 <script>
+import { ref } from "@vue/reactivity";
 export default {
+	setup() {
+		const title = ref("");
+		const description = ref("");
 
-}
+		const handleSubmit = () => {
+			console.log(title.value, description.value);
+		};
+
+		return { title, description, handleSubmit };
+	},
+};
 </script>
 
 <style>
-
+form {
+	background: white;
+}
+input[type="file"] {
+	border: 0;
+	padding: 0;
+}
+label {
+	font-size: 12px;
+	display: block;
+	margin-top: 30px;
+}
+button {
+	margin-top: 20px;
+}
 </style>
