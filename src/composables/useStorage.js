5599b0f40ac5500610dbc43caa6032366eebe6b2
John Doe <kurbanof.atabek@gmail.com>
2021-07-29 18:29:06 +0600

    Created useStorage composable

---
+++ src/composables/useStorage.js
@@ -0,0 +1,28 @@
+import { ref } from 'vue'
+import { storage } from '../firebase/config'
+import getUser from './getUser'
+
+const { user } = getUser()
+
+const useStorage = () => {
+	const error = ref(null)
+	const url = ref(null)
+	const filePath = ref(null)
+
+	const uploadImage = async (file) => {
+		filePath.value = `covers/${user.value.uid}/${file.name}`
+		const storageRef = storage.ref(filePath.value)
+
+		try {
+			const res = await storageRef.put(file)
+			url.value = res.ref.getDownloadURL()
+		} catch (err) {
+			console.log(err.message)
+			error.value = err.message
+		}
+	}
+
+	return { url, filePath, error, uploadImage }
+}
+
+export default useStorage
