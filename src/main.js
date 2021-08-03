1ea9563b28aa692c27499b49281fcdd6c5a59078
John Doe <kurbanof.atabek@gmail.com>
2021-08-03 18:18:57 +0600

    add authstateChanged for waiting answer from firebase

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { auth } from '../firebase/config'
 
 import './assets/main.css'
 
-createApp(App)
+let app;
+
+auth.onAuthStateChanged(() => {
+  app = createApp(App)
   .use(router)
   .mount('#app')
+})
