f5b74e2f81291efbd475033a351bcfbc760350af
John Doe <kurbanof.atabek@gmail.com>
2021-08-03 17:32:58 +0600

    created ListView component, nested it in Home.vue, add styles for ListView

---
+++ src/components/ListView.vue
@@ -0,0 +1,58 @@
+<template>
+  <div v-for="playlist in playlists" :key="playlist.id">
+    <div class="single">
+      <div class="thumbnail">
+        <img :src="playlist.coverUrl" :alt="playlist.title">
+      </div>
+      <div class="info">
+			  <h3>{{playlist.title}}</h3>
+			  <p>Created By {{playlist.userName}}</p>
+		  </div>
+      <div class="song-number">
+        <p>{{playlist.songs.length}}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props:['playlists']
+}
+</script>
+
+<style scoped>
+  .single {
+    display: flex;
+    align-items: center;
+    padding: 20px;
+    border-radius: 10px;
+    background: white;
+    margin: 16px 0;
+    transition: all ease 0.2s;
+  }
+  .single:hover {
+    box-shadow: 1px 2px 3px rgba(50,50,50,0.05);
+    transform: translateY(-5px);
+    transition: all ease 0.2s;
+  }
+  .thumbnail {
+    max-width: 100px;
+    max-height: 100px;
+    overflow: hidden;
+    border-radius: 10px;
+  }
+  img {
+    max-width: 150%;
+    max-height: 150%;
+    width: 100px;
+    height: 100px;
+    display: block;
+  }
+  .info {
+    margin: 0 30px;
+  }
+  .song-number {
+    margin-left: auto;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,20 +2,19 @@
 	<div class="home">
 		<div v-if="error" class="error">Could not fetch the data</div>
 		<div v-if="documents">
-			<div v-for="doc in documents" :key="doc.id">
-				{{ doc.title }}
-			</div>
+			<ListView :playlists="documents" />
 		</div>
 	</div>
 </template>
 
 <script>
+import ListView from "@/components/ListView.vue";
 import getColection from "../composables/getCollection";
 export default {
 	name: "Home",
+	components: {ListView},
 	setup() {
-		const { error, documents } = getColection('playlists');
-
+		const { error, documents } = getColection("playlists");
 		return { error, documents };
 	},
 };
