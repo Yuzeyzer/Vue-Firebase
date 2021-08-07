c39012967d4dc5f025ebaf3b427ad1630e64e6e4
John Doe <kurbanof.atabek@gmail.com>
2021-08-07 17:20:45 +0600

    created getDocument composable

---
+++ src/composables/getDocument.js
@@ -0,0 +1,34 @@
+import { ref, watchEffect } from 'vue'
+import { firestore } from '../firebase/config'
+
+const getDocument = (collection,id) => {
+  const document = ref(null)
+  const error = ref(null)
+
+  // register the firestore collection reference
+  let documentRef = firestore.collection(collection).doc(id)
+
+  const unsub = documentRef.onSnapshot(
+    (snap) => {
+      if (doc.data()) {
+        document.value =  {...doc.data(), id: doc.id}
+        error.value = null
+      }
+      else {
+        error.value = 'that document dost not exist'
+      }
+    },
+    (err) => {
+      document.value = null
+      error.value = `could not fetch the data, ${err.message}`
+    },
+  )
+
+  watchEffect((onInvalidate) => {
+    onInvalidate(() => unsub())
+  })
+
+  return { error, document }
+}
+
+export default getDocument
